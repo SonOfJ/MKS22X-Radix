152d943d627a6cb8f489e5772b3480701b2d6f73
Jason Zheng <zzheng3@stuy.edu>
2019-04-01 14:49:19 -0400

    Stopping here LOL.

--- Radix.java
+++ Radix.java
@@ -1,5 +1,10 @@
 public class Radix {
-  public static void radixsort(int[]data) {
-
+  public static void radixsort(int[] data) {
+    int max = data[0]; //Variable that points to the highest value in a list.
+    for (int i = 0; i < data.length; i = i + 1) {
+      if (data[i] > max) {
+        max = data[i];
+      }
+    }
   }
 }
