dae86828c56acc127aac0f551b9f200a00550c91
Jason Zheng <zzheng3@stuy.edu>
2019-04-02 14:47:37 -0400

    TIRED AND CONFUSED.

--- Radix.java
+++ Radix.java
@@ -1,10 +1,13 @@
 public class Radix {
   public static void radixsort(int[] data) {
     int max = data[0]; //Variable that points to the highest value in a list.
-    for (int i = 0; i < data.length; i = i + 1) {
+    for (int i = 0; i < data.length; i = i + 1) { //Find the max value.
       if (data[i] > max) {
         max = data[i];
       }
+    }
+    for (int i = 1; max / i > 0; i = i * 10) { //Count the number of digits.
+
     }
   }
 }
