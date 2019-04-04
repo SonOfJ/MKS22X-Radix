f13fefd188e1c2189226e6a5cb05125251a11137
Jason Zheng <zzheng3@stuy.edu>
2019-04-04 02:28:05 -0400

    Working on while loop right now.

--- Radix.java
+++ Radix.java
@@ -1,13 +1,22 @@
 public class Radix {
   public static void radixsort(int[] data) {
     @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for (int i = 0; i < 20; i = i + 1) {
+      buckets[i] = new MyLinkedList<>();
+    }
     int max = data[0]; //Variable that points to the highest value in a list.
     for (int i = 0; i < data.length; i = i + 1) { //Find the max value.
-      if (data[i] > max) {
-        max = data[i];
+      if (Math.abs(data[i]) > max) {
+        max = Math.abs(data[i]);
       }
     }
-    for (int i = 1; max / i > 0; i = i * 10) { //Count the number of digits.
+    max = (int)Math.log10(max) + 1; //Get the total number of digits for the maximum value.
+    int index = 0; //This will go through the place values.
+    
+    while (index < max) {
+
+    }
 
     }
   }
