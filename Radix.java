129ebe389138177aafc4b9664adbea286199e6b8
Jason Zheng <zzheng3@stuy.edu>
2019-04-04 03:18:51 -0400

    Think I should be done.

--- Radix.java
+++ Radix.java
@@ -33,9 +33,13 @@ public class Radix {
           }
         }
       }
-
+      for (int i = 0; i < 20; i = i + 1) {
+        temp.extend[buckets[i]]; //Add on the buckets.
+      }
+      index = index + 1; //Make sure the place index keeps moving.
     }
-
+    for (int i = 0; i < data.length; i = i + 1) {
+      data[i] = temp.removeFront(); //Put everything back into the original array.
     }
   }
 }
