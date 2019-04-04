b4904e8cbb9a730d7ac1830bd2e7863134c83fd3
Jason Zheng <zzheng3@stuy.edu>
2019-04-04 18:05:31 -0400

    Please tell me it works this time.

--- Radix.java
+++ Radix.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class Radix {
   public static void radixsort(int[] data) {
     @SuppressWarnings("unchecked")
@@ -6,41 +5,38 @@ public class Radix {
     for (int i = 0; i < 20; i = i + 1) {
       buckets[i] = new MyLinkedList<>();
     }
+    MyLinkedList<Integer> temp = new MyLinkedList<>(); //List of temporary values.
     int max = data[0]; //Variable that points to the highest value in a list.
     for (int i = 0; i < data.length; i = i + 1) { //Find the max value.
-      if (Math.abs(data[i]) > max) {
-        max = Math.abs(data[i]);
+      if (data[i] > max) {
+        max = data[i];
       }
     }
-    max = (int)Math.log10(max) + 1; //Get the total number of digits for the maximum value.
-    MyLinkedList<Integer> temp = new MyLinkedList<>(); //List of temporary values.
-    int index = 0; //This will go through the place values.
-    while (index < max) {
-      if (index == 0) { //Ones place.
-        for (int i = 0; i < data.length; i = i + 1) {
-          if (data[i] % 10 >= 0) { //Positive number.
-            buckets[10 + data[i] % 10].add(data[i]); //Put it in between 10 and 19.
-          } else { //Negative number.
-            buckets[9 + data[i] % 10].add(data[i]); //Put it in between 0 and 9.
-          }
+    String digits = "" + max;
+    int runs = digits.length() - 1; //Number of passes needed.
+    for (int i = 0; i < runs; i = i + 1) {
+      if (i == 0) { //First value.
+        for (int j = 0; j < data.length; j = j + 1) {
+          buckets[data[i] % 10 + 9].add(data[i]);
         }
       } else {
-        while (temp.size() > 0) { //There is something in the linked list.
-          int value = temp.removeFront();
-          if ((int)(value / Math.pow(10, index)) % 10 >= 0) { //If the digit is positive...
-            buckets[10 + (int)(value / Math.pow(10, index)) % 10].add(value); //Put it between 10 and 19.
-          } else { //If the digit is negative...
-            buckets[9 + (int)(value / Math.pow(10, index)) % 10].add(value); //Put it between 0 and 9.
-          }
+        buckets[temp.start().getData() / Math.pow(10, i) % 10].add(temp.start().getData());
+        while (temp.start().hasNext()) {
+          temp.start() = temp.start().next();
+          buckets[temp.start().getData() / Math.pow(10, i) % 10].add(temp.start().getData());
         }
       }
-      for (int i = 0; i < 20; i = i + 1) {
-        temp.extend(buckets[i]); //Add on the buckets.
+      temp.clear();
+      for (int j = 0; j < 20; j = j + 1) {
+        temp.extend(buckets[j]);
       }
-      index = index + 1; //Make sure the place index keeps moving.
     }
-    for (int i = 0; i < data.length; i = i + 1) {
-      data[i] = temp.removeFront(); //Put everything back into the original array.
+    data[0] = temp.start().getData();
+    int index = 1;
+    while (temp.start().hasNext()) {
+      temp.start() = temp.start().next();
+      data[i] = temp.start().getData();
+      i = i + 1;
     }
   }
 }
