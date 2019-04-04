70ba4f7b99d9d36e4bae19338f2bfd8ce6a7db1a
Jason Zheng <zzheng3@stuy.edu>
2019-04-04 02:15:08 -0400

    Suppress warnings.

--- Radix.java
+++ Radix.java
@@ -1,5 +1,6 @@
 public class Radix {
   public static void radixsort(int[] data) {
+    @SuppressWarnings("unchecked")
     int max = data[0]; //Variable that points to the highest value in a list.
     for (int i = 0; i < data.length; i = i + 1) { //Find the max value.
       if (data[i] > max) {
