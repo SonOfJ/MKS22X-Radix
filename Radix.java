c33d74f321e796fd42fb7bbe274ad60e0fb20f38
Jason Zheng <zzheng3@stuy.edu>
2019-04-04 03:11:34 -0400

    More work on while loop.

--- Radix.java
+++ Radix.java
@@ -13,15 +13,26 @@ public class Radix {
     }
     max = (int)Math.log10(max) + 1; //Get the total number of digits for the maximum value.
     MyLinkedList<Integer> temp = new MyLinkedList<>(); //List of temporary values.
-    for (int i = 0; i < data.length; i = i + 1) {
-      if (data[i] % 10 >= 0) { //Positive number.
-        buckets[10 + data[i] % 10].add(data[i]);
-      } else { //Negative number.
-        buckets[9] + data[i] % 10].add(data[i]);
-      }
-    }
-    int index = 1; //This will go through the place values.
+    int index = 0; //This will go through the place values.
     while (index < max) {
+      if (index == 0) { //Ones place.
+        for (int i = 0; i < data.length; i = i + 1) {
+          if (data[i] % 10 >= 0) { //Positive number.
+            buckets[10 + data[i] % 10].add(data[i]);
+          } else { //Negative number.
+            buckets[9] + data[i] % 10].add(data[i]);
+          }
+        }
+      } else {
+        while (temp.size() > 0) { //There is something in the linked list.
+          int 
+          if ((int)(temp.removeFront() / Math.pow(10, index)) % 10 >= 0) { //If the digit is positive...
+            buckets[10 + (int)(temp.removeFront() / Math.pow(10, index))].add(data.removeFront()); //Put it between 10 and 19.
+          } else { //If the digit is negative...
+            buckets[9 + (int)(temp.removeFront() / Math)]
+          }
+        }
+      }
 
     }
 
